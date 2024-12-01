84ce86417e8206b1abf7ea811d1cd6c61fe94143
CaioGMorata <51411417+CaioGMorata@users.noreply.github.com>
2024-12-01 19:42:21 -0300

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -13,10 +13,10 @@ ON
     s.Value BETWEEN n.Min_Value AND n.Max_Value
 ORDER BY 
     CASE 
-        WHEN n.Grade >= 8 THEN -n.Grade -- Ordem decrescente para notas >= 8
-        ELSE -n.Grade                   -- Ordem crescente para notas < 8
+        WHEN n.Grade >= 8 THEN -n.Grade -- Ordem decrescente para notas >= 8. Descending order for grades >= 8.
+        ELSE -n.Grade                   -- Ordem crescente para notas < 8. Ascending order for grades < 8.
     END,
     CASE 
-        WHEN n.Grade >= 8 THEN s.Name  -- Ordem alfabética para nomes (grade >= 8)
-        ELSE s.Value                   -- Ordem crescente para notas < 8
+        WHEN n.Grade >= 8 THEN s.Name  -- Ordem alfabética para nomes (grade >= 8). Alphabetical order for names (grid >= 8).
+        ELSE s.Value                   -- Ordem crescente para notas < 8. Ascending order for grades < 8.
     END;
